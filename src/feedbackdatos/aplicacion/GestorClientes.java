520cf2003c24168e3e4aef2d24a35b1a75ea04bc
YholandaSM <ysoberonmunduate@gmail.com>
2019-12-17 18:49:35 +0100

    Modificación gestorClientes y GestorCoches

--- src/feedbackdatos/aplicacion/GestorClientes.java
+++ src/feedbackdatos/aplicacion/GestorClientes.java
@@ -2,6 +2,7 @@ package feedbackdatos.aplicacion;
 
 import feedbackdatos.Clientes;
 import feedbackdatos.HibernateUtil;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,15 +13,14 @@ import org.hibernate.exception.ConstraintViolationException;
  * @author Yolanda
  */
 public class GestorClientes {
-    
-    
+
     /**
-     * 
+     *
      * @param dni
      * @param nombre
      * @param apellidos
      * @param direc
-     * @param telefono 
+     * @param telefono
      */
     public void altaCliente(String dni, String nombre, String apellidos,
             String direc, String telefono) {
@@ -31,16 +31,15 @@ public class GestorClientes {
         Transaction trans = sesion.beginTransaction();
 
         System.out.println("Insertamos una fila en la tabla CLIENTES...");
-        
-        Clientes cliente= new Clientes();
+
+        Clientes cliente = new Clientes();
         cliente.setIdCliente(new GetId().getIdCliente());
         cliente.setDni(dni);
         cliente.setNombre(nombre);
         cliente.setApellidos(apellidos);
         cliente.setDireccion(direc);
         cliente.setTelefono(telefono);
-        
-        
+
         try {
 
             sesion.save(cliente);
@@ -54,18 +53,84 @@ public class GestorClientes {
             System.exit(0);
 
         }
-        
-        
 
     }
-    
-    
-    
-    public void bajaCoche(){
-        
-        
-        
-        
+
+    /**
+     *
+     * @param id
+     */
+    public void bajaCliente(int id) {
+
+        SessionFactory factoria = HibernateUtil.getSessionFactory();
+        Session sesion = factoria.openSession();
+
+        Transaction trans = sesion.beginTransaction();
+
+        //Recuperamos el cliente que queremos borrar
+        Clientes cliente = (Clientes) sesion.load(Clientes.class, id);
+
+        try {
+            sesion.delete(cliente);
+            trans.commit();
+            System.out.println("Cliente eliminado: " + id);
+        } catch (ObjectNotFoundException o) {
+
+            System.out.println("Error: no existe el cliente: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println("Error");
+            e.printStackTrace();
+
+        }
+
+        sesion.close();
+        System.exit(0);
+
+    }
+
+    /**
+     *
+     * @param id
+     * @param direccion
+     * @param telefono
+     */
+    public void modificarCliente(int id, String direccion, String telefono) {
+
+        SessionFactory factoria = HibernateUtil.getSessionFactory();
+        Session sesion = factoria.openSession();
+
+        Transaction trans = sesion.beginTransaction();
+        try {
+            //Recuperamos el cliente que vamos a modificar
+            Clientes cliente = (Clientes) sesion.load(Clientes.class, id);
+            System.out.printf("Modificamos el cliente: %d%n", cliente.getIdCliente());
+            System.out.printf("Dirección anterior: %s%n", cliente.getDireccion());
+            System.out.printf("Teléfono anterior: %s%n", cliente.getTelefono());
+
+            cliente.setDireccion(direccion);
+            cliente.setTelefono(telefono);
+
+            sesion.update(id);
+            trans.commit();
+            System.out.printf("Dirección nueva: %s%n", cliente.getDireccion());
+            System.out.printf("Teléfono nuevo: %s%n", cliente.getTelefono());
+
+        } catch (ObjectNotFoundException o) {
+
+            System.out.println("Error: no existe el cliente: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println("Error");
+            e.printStackTrace();
+
+        }
+
+        sesion.close();
+        System.exit(0);
+
     }
 
 }
--- src/feedbackdatos/aplicacion/GestorCoches.java
+++ src/feedbackdatos/aplicacion/GestorCoches.java
@@ -5,10 +5,11 @@
  */
 package feedbackdatos.aplicacion;
 
+import feedbackdatos.Clientes;
 import feedbackdatos.Coches;
 import feedbackdatos.HibernateUtil;
 import java.util.Date;
-import java.util.GregorianCalendar;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -54,4 +55,80 @@ public class GestorCoches {
         }
 
     }
+
+    public void bajaCoche(int id) {
+
+        SessionFactory factoria = HibernateUtil.getSessionFactory();
+        Session sesion = factoria.openSession();
+
+        Transaction trans = sesion.beginTransaction();
+
+        //Recuperamos el coche que queremos borrar
+        Coches coche = (Coches) sesion.load(Coches.class, id);
+
+        try {
+            sesion.delete(coche);
+            trans.commit();
+            System.out.println("Coche eliminado: " + id);
+        } catch (ObjectNotFoundException o) {
+
+            System.out.println("Error: no existe el coche: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println("Error");
+            e.printStackTrace();
+
+        }
+
+        sesion.close();
+        System.exit(0);
+
+    }
+
+    public void modificarCoche(int id, float precio, String color, String marca,
+            Date fecha) {
+
+        SessionFactory factoria = HibernateUtil.getSessionFactory();
+        Session sesion = factoria.openSession();
+
+        Transaction trans = sesion.beginTransaction();
+        try {
+            //Recuperamos el coche que vamos a modificar
+            Coches coche = (Coches) sesion.load(Coches.class, id);
+            System.out.printf("Modificamos el coche: %d%n", coche.getIdCoche());
+            System.out.printf("Precio anterior: %.2f%n", coche.getPrecio());
+            System.out.printf("Color anterior: %s%n", coche.getColor());
+            System.out.printf("Marca anterior: %s%n", coche.getMarca());
+            System.out.printf("Fecha anterior: %d%n", coche.getFechaMatriculacion());
+
+            coche.setPrecio(precio);
+            coche.setColor(color);
+            coche.setMarca(marca);
+            coche.setFechaMatriculacion(fecha);
+
+            sesion.update(id);
+            trans.commit();
+
+            System.out.printf("Precio nuevo: %.2f%n", coche.getPrecio());
+            System.out.printf("Color nuevo: %s%n", coche.getColor());
+            System.out.printf("Marca nuevo: %s%n", coche.getMarca());
+            System.out.printf("Fecha nuevo: %d%n", coche.getFechaMatriculacion());
+
+        } catch (ObjectNotFoundException o) {
+
+            System.out.println("Error: no existe el cliente: " + id);
+
+        } catch (Exception e) {
+
+            System.out.println("Error");
+            e.printStackTrace();
+
+        }
+
+        sesion.close();
+        System.exit(0);
+
+    }
+
 }
